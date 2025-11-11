402ce7d06b4c7af110c6b0f9752aa503f514664a
Bouhenna Othmane <bouhenna.othmane@gmail.com>
2025-11-11 22:55:45 +0100

    progress

---
+++ src/services/useAccountService.js
@@ -0,0 +1,43 @@
+// src/services/useAccountService.js
+import apiClient from '@/services/axios';
+
+export const useAccountService = {
+    async getAccounts(params) {
+        try {
+            const response = await apiClient.post('/api/admin/accounts/filter', { params });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async storeAccount(accountData) {
+        try {
+            await apiClient.get('/sanctum/csrf-cookie');
+            const response = await apiClient.post('/api/admin/accounts', accountData);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async updateAccount(accountId, updatedData) {
+        try {
+            await apiClient.get('/sanctum/csrf-cookie');
+            const response = await apiClient.put(`/api/admin/accounts/${accountId}`, updatedData);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    async deleteAccounts(accountsIds) {
+        try {
+            await apiClient.get('/sanctum/csrf-cookie');
+            const response = await apiClient.delete('/api/admin/accounts', { data: { accounts: accountsIds } });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+};
